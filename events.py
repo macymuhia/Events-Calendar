dcf4a52dfb382a616818441d1bfe76155652280b
Mercy Muhia <mercy8muhia@gmail.com>
2018-11-14 13:05:41 +0300

    Changed each calendar option to be in its own function

--- events.py
+++ events.py
@@ -1,9 +1,10 @@
 """
 A calendar that a user can view, add event, update event or delete event
 """
-from time import sleep, strftime, gmtime
+from time import sleep, strftime
 
 calendar = {}
+
 
 def welcome():
   USER_FIRST_NAME = raw_input("What's your name? ")
@@ -15,54 +16,70 @@ def welcome():
   sleep(1)
   print "What would you like to do?"
 
+def view_calendar():
+  if len(calendar.keys()) < 1:
+    print "Calendar empty."
+  else:
+    print calendar
+
+def update_calendar():
+  date = raw_input("What date? ")
+  update = raw_input("Enter the update: ")
+  calendar[date] = update
+  print ("Update successful!")
+  print calendar
+
+def add_calendar():
+  event = raw_input("Enter event: ")
+  date = raw_input("Enter date (MM/DD/YYYY): ")
+  if (len(date) > 10 or int(date[6:10]) < int(strftime("%Y"))):
+    print "invalid date"
+    #date = raw_input("Enter date (MM/DD/YYYY): ")
+    try_again = raw_input("Try again? Y for Yes, N for No: ").upper()
+    if try_again == "Y":
+      start_calendar()
+    else:
+      start = False
+  else:
+    calendar[date] = event
+    print ("Event added successfully!")
+    print calendar
+
+def delete_calendar():
+  if len(calendar.keys()) < 1:
+    print ("Calendar is empty!")
+  else:
+    event = raw_input("What event? ")
+    for date in calendar.keys():
+      if event == calendar[date]:
+        del calendar[date]
+        print ("Event successfully deleted! ")
+        print calendar
+            
+      else:
+        print ("Incorrect event specified! ")
+
 def start_calendar():
+  
   welcome()
   start = True
+  
   while start:
+    
     user_choice = raw_input("Select A to Add, U to Update, V to View, D to Delete or X to Exit: ").upper()
     
     if user_choice == "V":
-      if len(calendar.keys()) < 1:
-        print "Calendar empty."
-      else:
-        print calendar
+      view_calendar()
     
     elif user_choice == "U":
-      date = raw_input("What date? ")
-      update = raw_input("Enter the update: ")
-      calendar[date] = update
-      print ("Update successful!")
-      print calendar
-      
+      update_calendar()
+    
     elif user_choice == "A":
-      event = raw_input("Enter event: ")
-      date = raw_input("Enter date (MM/DD/YYYY): ")
-      if (len(date) > 10 or int(date[6:10]) < int(strftime("%Y"))):
-        print "invalid date"
-        date = raw_input("Enter date (MM/DD/YYYY): ")
-        try_again = raw_input("Try again? Y for Yes, N for No: ").upper()
-        if try_again == "Y":
-          continue
-        else:
-          start = False
-      else:
-        calendar[date] = event
-        print ("Event added successfully!")
-        print calendar
+      add_calendar()
+    
     elif user_choice == "D":
-      if len(calendar.keys()) < 1:
-        print ("Calendar is empty!")
-      else:
-        event = raw_input("What event? ")
-        for date in calendar.keys():
-          if event == calendar[date]:
-            del calendar[date]
-            print ("Event successfully deleted! ")
-            print calendar
-            
-          else:
-            print ("Incorrect event specified! ")
-            
+      delete_calendar()
+
     elif user_choice == "X":
       start = False
       
